dab4fbc36abaf9e3fdfe6346fd43cf75bd024e68
suhas <suhaskhutale1717@gmail.com>
2025-06-27 15:38:44 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,29 @@ import ContactPage from "./pages/ContactPage"
 import AdmissionPage from "./pages/AdmissionPage"
 import CoursesPage from "./pages/CoursesPages"
 import ChatbotComponent from "./components/Chatbot/ChatbotComponent"
+import DeveloperInfoPopup from "./components/DeveloperInfo/DeveloperInfoPopup"
+import { useState } from "react"
 
 // import './styles/Pages.css'
 
 
 const App = () => {
+  const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
   return (
-   <>
+    <>
+   <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Suhas krishna khutale"
+          studentPhotoUrl="/images/suhas.jpg" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
    <Router>
     <Routes>
       <Route path="/" element={<HomePage/>}/>
